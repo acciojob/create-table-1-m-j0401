c776faf5a205bd3be376a78639808673f73578e0
m-j0401 <mehul.jain2206@gmail.com>
2026-02-03 13:11:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
+  let table=document.querySelector("#sampleTable");
+	let tr=document.createElement("tr");
+	let td_1=document.createElement("td");
+	let td_2=document.createElement("td");
+	td_1.innerText="New Cell1"
+	td_2.innerText="New Cell2"
+	tr.append(td_1,td_2);
+	table.prepend(tr);
   
 }
